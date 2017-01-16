bbae368828c05774fc522ca69eb8b2c7c67a55bb
john unwin <jdunwin@gmail.com>
2017-01-15 22:31:17 -0600

    added bar chats

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # AlexTest
 Simple test grid - essentially a repo where I play with Angular 1
+
+Install
+You need to have NPM installed
+
+npm install angular-ui-grid
+npm install angular-chart.js --save
+npm install chart.js --save
+npm install
+
+
+Debuggging in Chrome using Visual Code
+You need to install the Chrome debugger
+* Ctrl+Shift + P to open command palatte
+* ext install
+* Install Debugger for Chrome - note you will probably need to restart visula studio code
+
+Running  
+npm run lite
--- alextest.js
+++ alextest.js
@@ -1,4 +1,4 @@
-var app = angular.module('alextest', ['ngTouch', 'ngAnimate', 'ui.bootstrap', 'ui.grid', 'ui.grid.cellNav', 'ui.grid.resizeColumns', 'ui.grid.autoResize']);
+var app = angular.module('alextest', ['ngTouch', 'ngAnimate', 'ui.bootstrap', 'ui.grid', 'ui.grid.cellNav', 'ui.grid.resizeColumns', 'ui.grid.autoResize', 'chart.js']);
 
 app.controller('AlexTestCtrl', ['$scope', '$http', '$log', function ($scope, $http, $log) {
 
@@ -24,7 +24,6 @@ app.controller('AlexTestCtrl', ['$scope', '$http', '$log', function ($scope, $ht
     // Defines a function to Load the data from your json file - this will only work from 
     // a web server
     this.getPerfData = function getPerfData() {
-
         return $http({
             method: "GET",
             url: "http://localhost:3000/alextestdata.json"
@@ -40,19 +39,32 @@ app.controller('AlexTestCtrl', ['$scope', '$http', '$log', function ($scope, $ht
         });
 
 
-/*
-
-        return $http.get('alextestdata.json').success(function (response) {
-            var dd = eval(response);
-            dd = dd.CommercialCompanies;
-            $scope.gridOptions.data.length = 0;
-            angular.forEach(dd, function (row) {
-                $scope.gridOptions.data.push(row);
-            });
-        });
-        */
+        /*
+        
+                return $http.get('alextestdata.json').success(function (response) {
+                    var dd = eval(response);
+                    dd = dd.CommercialCompanies;
+                    $scope.gridOptions.data.length = 0;
+                    angular.forEach(dd, function (row) {
+                        $scope.gridOptions.data.push(row);
+                    });
+                });
+                */
     };
+
+
+
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.series = ['Series A', 'Series B'];
+
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+
 
     // Call the function that gets the data
     this.getPerfData();
+
+
 }]);
